9607241d9eb514a6e5d9da5c288b694bbc4fbb1a
Shashindu <shashinduchamika99@gmail.com>
2023-07-11 00:21:17 +0530

    sn7

--- sn-care/src/component/sidebar/SideBar.js
+++ sn-care/src/component/sidebar/SideBar.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import './SideBar.css'
 import logo from './images/logo (2).png'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function SideBar({isClicked}) {
   const [sideBarStyle, setSideBarStyle] = useState('hide-side-bar')
+  const [isNavLinkNumber,setIsNavLinkNumber] = useState(null)
   
+  const location = useLocation()
+  const currentPath = location.pathname
+
+  const handleNavItem = (index)=>{
+     //alert(currentPath)
+     setIsNavLinkNumber(index)
+
+  }
+
   useEffect(()=>{
     if(isClicked){
         setSideBarStyle('display-side-bar')
@@ -21,13 +31,34 @@ export default function SideBar({isClicked}) {
       <div className='logo-part'>
          <img src={logo} width='60%' alt='side-bar-logo'></img>
       </div>
-      <div className='navigation-links'>
+      <div className='navigation-links '>
          <ul className='slide-bar-links'>
-            <li className='slide-bar-link'><a href='/'>Home</a></li>
-            <li className='slide-bar-link'><a href='/'>Department</a></li>
-            <li className='slide-bar-link'><a href='#why-choosing'>About</a></li>
-            <li className='slide-bar-link'><a href='#contact-us'>Contact</a></li>
-            <li className='slide-bar-link'><Link to='/login'>Log in</Link></li>
+
+            <li className={isNavLinkNumber === 1 && currentPath ==='/'?'slide-bar-link-active':'slide-bar-link'} 
+              onClick={()=>handleNavItem(1)}>
+              <Link className={isNavLinkNumber === 1 && currentPath ==='/'?'s-b-link-active':'s-b-link'} to='/'>Home</Link>
+            </li>
+
+            <li className={isNavLinkNumber === 2 && currentPath ==='/'?'slide-bar-link-active':'slide-bar-link'} 
+              onClick={()=>handleNavItem(2)}>
+              <Link className={isNavLinkNumber === 2 ?'s-b-link-active':'s-b-link'} to='/'>About</Link>
+            </li>
+
+            <li className={isNavLinkNumber === 3 && currentPath ==='/'?'slide-bar-link-active':'slide-bar-link'} 
+              onClick={()=>handleNavItem(3)}>
+              <Link className={isNavLinkNumber === 3 && currentPath ==='/' ?'s-b-link-active':'s-b-link'} to='/'>Contact</Link>
+            </li>
+
+            <li className={isNavLinkNumber === 4 && currentPath ==='/'?'slide-bar-link-active':'slide-bar-link'} 
+              onClick={()=>handleNavItem(4)}>
+              <Link className={isNavLinkNumber === 4 && currentPath ==='/'?'s-b-link-active':'s-b-link'} to='/'>Department</Link>
+            </li>
+
+            <li className={isNavLinkNumber === 5 && currentPath ==='/' ?'slide-bar-link-active':'slide-bar-link'} 
+              onClick={()=>handleNavItem(5)}>
+              <Link className={isNavLinkNumber === 5 && currentPath ==='/'?'s-b-link-active':'s-b-link'} to='/login'>Login</Link>
+            </li>
+
          </ul>
       </div>
     </div>
